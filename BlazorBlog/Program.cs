e3e0a326f2a9c3b867f9babb897e4498e27a646d
Baba Kamyljanov <babakamyljanov@ssw.com.au>
2024-11-01 15:48:50 +0800

    added yaml frontmatter

--- BlazorBlog/Program.cs
+++ BlazorBlog/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddScoped(sp =>
     new MarkdownPipelineBuilder()
     .UseAdvancedExtensions()
+    .UseYamlFrontMatter()
     .UseSyntaxHighlighting()
     .Build());
 
